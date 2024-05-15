205f2fb7011c6353eaa9f405b07bfba77862d7c3
phannamqt <phannam88@gmail.com>
2024-05-15 09:20:28 +0700

    Update websocket.gateway.ts

--- src/websocket/websocket.gateway.ts
+++ src/websocket/websocket.gateway.ts
@@ -36,36 +36,27 @@ export class WebsocketGateway
   }
 
   async sendMessage(room: string, data: any) {
-    console.log(`Attempting to send message to room ${room} with data:`, data);
     this.log(`Attempting to send message to room ${room} with data: ${data}`);
     const clients = await this.server.in(room).fetchSockets();
     if (clients.length > 0) {
       this.server.to(room).emit('message', data);
-      console.log(`Message sent to room ${room}`);
-      this.log(`Message sent to room ${room}`);
+      this.log(`Message sent to room ${room}: ${data}`);
     } else {
-      console.log(`Room ${room} does not exist or has no clients`);
-      this.log(`Room ${room} does not exist or has no clients`);
+      this.log(`Room ${room} does not exist or has no clients, with data: ${data}`);
     }
   }
 
   @SubscribeMessage('joinRoom')
   async joinRoom(client: Socket, data: any): Promise<any> {
     if (data) {
-      console.log(
-        `Client ${client.id} is attempting to join room ${data?.room} with data:`,
-        data,
-      );
+      this.log(`Client ${client.id} is attempting to join room ${data?.room} with data: ${JSON.stringify(data)}`);
       try {
         client.join(data?.room);
-        this.log(`Client ${client.id} successfully joined room ${data?.room}`);
+        this.log(`Client ${client.id} successfully joined room ${data?.room} with data: ${JSON.stringify(data)}`);
         this.server.to(data?.room).emit('joined', data);
-        console.log('Emitted joined event to room', data?.room);
-        this.log('Emitted joined event to room' + data?.room);
+        this.log(`Emitted joined event to room ${data?.room} with data: ${data}`);
       } catch (error) {
-        this.log(
-          `Client ${client.id} failed to join room ${data?.room}: ${error}`,
-        );
+        this.log(`Client ${client.id} failed to join room ${data?.room}: ${error}`);
       }
     }
   }
