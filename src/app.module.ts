1c3c99f3866aac7056adb5c6f0efd43cb9851855
Phan Nam <phannam88@gmail.com>
2024-05-21 11:46:55 +0700

    git add code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { WebsocketGateway } from './websocket/websocket.gateway';
+import { SocketModule } from './socket/socket.module';
+import { CacheModule, CacheModuleAsyncOptions } from '@nestjs/cache-manager';
+import { redisStore } from 'cache-manager-redis-store';
+import { UserModule } from './modules/user/user.module';
+
+export const RedisOptions: CacheModuleAsyncOptions = {
+  isGlobal: true,
+  useFactory: async () => {
+    const store = await redisStore({
+      url: `redis://redis:6379`,
+    });
+    return {
+      store: () => store,
+      ttl: 86400000, // 1 Day = 24 * 60 * 60 * 1000 = 86,400,000 milliseconds.
+    };
+  },
+};
 
 @Module({
-  imports: [],
+  imports: [SocketModule, CacheModule.registerAsync(RedisOptions), UserModule],
   controllers: [AppController],
-  providers: [AppService, WebsocketGateway],
+  providers: [AppService],
 })
 export class AppModule {}
